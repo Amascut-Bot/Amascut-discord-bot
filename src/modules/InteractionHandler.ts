82bb501b41862dd1f90769c0396ea404a695f8b4
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2025-08-19 11:59:41 -0600

    feat: editor role added to interaction handler

--- src/modules/InteractionHandler.ts
+++ src/modules/InteractionHandler.ts
@@ -188,6 +188,15 @@ export default class InteractionHandler extends EventEmitter {
                             return await interaction.reply({ content: 'You do not have permissions to run this command. This incident has been logged.', ephemeral: true });
                         }
                         break;
+                    case 'EDITOR':
+                        if (!(await this.client.util.hasRolePermissions(this.client, ['editor', 'admin', 'owner'], interaction))) {
+                            this.client.logger.log({
+                                message: `Attempted restricted permissions. { command: ${command.name}, user: ${interaction.user.username}, channel: ${interaction.channel} }`,
+                                handler: this.constructor.name,
+                            }, true);
+                            return await interaction.reply({ content: 'You do not have permissions to run this command. This incident has been logged.', ephemeral: true });
+                        }
+                        break;
                     case 'TRIAL_TEAM_AND_TEACHER':
                         if (!(await this.client.util.hasRolePermissions(this.client, ['teacher', 'trialTeam', 'admin', 'owner'], interaction))) {
                             this.client.logger.log({
